9801b58e4b85bec5bc35a121d13d6f23e344a02a
Vitalie Spinu <spinuvit@gmail.com>
2018-09-06 02:28:03 +0200

    Call pm-map-over-spans with argument

--- tests/poly-rst-tests.el
+++ tests/poly-rst-tests.el
@@ -5,9 +5,9 @@
 (ert-deftest poly-rst/spans-at-borders ()
   (pm-test-run-on-file poly-rst-mode "code-blocks.rst"
     (pm-map-over-spans
-     (lambda ()
-       (let* ((sbeg (nth 1 *span*))
-              (send (nth 2 *span*))
+     (lambda (span)
+       (let* ((sbeg (nth 1 span))
+              (send (nth 2 span))
               (range1 (pm-innermost-range sbeg))
               (range2 (pm-innermost-range send)))
          (should (eq sbeg (car range1)))
@@ -18,8 +18,8 @@
 (ert-deftest poly-rst/spans-at-narrowed-borders ()
   (pm-test-run-on-file poly-rst-mode "code-blocks.rst"
     (pm-map-over-spans
-     (lambda ()
-       (pm-with-narrowed-to-span *span*
+     (lambda (span)
+       (pm-with-narrowed-to-span span
          (let* ((range1 (pm-innermost-range (point-min)))
                 (range2 (pm-innermost-range (point-max))))
            (should (eq (car range1) (point-min)))
