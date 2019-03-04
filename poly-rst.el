233b12b2be575158668a27ead91a55ad0f82b309
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2019-03-04 08:45:35 +0100

    Added package commentary.

--- poly-rst.el
+++ poly-rst.el
@@ -30,6 +30,10 @@
 ;;
 ;;; Commentary:
 ;;
+;; Polymode for working with files using ReStructured Text (ReST or rst) markup
+;; syntax.  This allows the user to work in regions marked with code using the
+;; appropriate Emacs major mode.
+;;
 ;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
 ;;; Code:
