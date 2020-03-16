8530f56fbdce01bcf4004839ff54e4156282c2b5
Vitalie Spinu <spinuvit@gmail.com>
2020-03-16 14:15:53 +0100

    Version 0.2.2

--- poly-rst.el
+++ poly-rst.el
@@ -2,8 +2,8 @@
 ;;
 ;; Author: Gustaf Waldemarson, Vitalie Spinu
 ;; Copyright (C) 2018 Gustaf Waldemarson, Vitalie Spinu
-;; Version: 0.2.1
-;; Package-Requires: ((emacs "25") (polymode "0.2.1"))
+;; Version: 0.2.2
+;; Package-Requires: ((emacs "25") (polymode "0.2.2"))
 ;; URL: https://github.com/polymode/poly-rst
 ;; Keywords: languages, multi-modes
 ;;
